07bcc316926cb8114bb0b04227b630dd28943cfd
ababab0020 <ababab0020@icloud.com>
2022-11-27 23:54:34 +0900

    destroy.html.erb

--- app/controllers/cinemas_controller.rb
+++ app/controllers/cinemas_controller.rb
@@ -12,6 +12,11 @@ class CinemasController < ApplicationController
     Cinema.create(cinema_params)
   end
 
+  def destroy
+    cinema = Cinema.find(params[:id])
+    cinema.destroy
+  end
+
   private
   def cinema_params
     params.require(:cinema).permit(:name, :image, :text)
